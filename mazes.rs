5e3df0ba9835b5832d86c9d8395f2cbf53202e2b
Chap Wilkinson <cement122293@gmail.com>
2017-04-19 03:40:41 -0500

    minor edits

--- mazes.rs
+++ mazes.rs
@@ -15,6 +15,7 @@ enum Direction {
     Right,
 }
 
+// TODO add wall semantics
 #[derive(Clone, Copy)]
 struct Node {
     up: Option<Cursor>,
@@ -25,25 +26,28 @@ struct Node {
 }
 
 impl Node {
+    // TODO add digraph styled pretty-printing
     fn print_node(&self) {
         println!("{4}\n{0}\n{1}\n{2}\n{3}",
-                 opening_to_string(self.up), opening_to_string(self.down),
-                 opening_to_string(self.left), opening_to_string(self.right),
+                 opening_to_string(self.up),
+                 opening_to_string(self.down),
+                 opening_to_string(self.left),
+                 opening_to_string(self.right),
                  node_type_to_string(self.n_type));
     }
 }
 
 fn opening_to_string(opening: Option<Cursor>) -> String {
-    let ret_string; 
+    let ret_string;
     match opening {
         Option::Some(cursor) => {
             ret_string = fmt::format(format_args!("Some(({}))", cursor.index));
             return ret_string;
-        },
+        }
         Option::None => {
             ret_string = fmt::format(format_args!("None"));
             return ret_string;
-        },
+        }
     }
 }
 
@@ -56,10 +60,14 @@ fn node_type_to_string<'a>(n_type: NodeType) -> &'a str {
 }
 
 #[derive(Clone, Copy, PartialEq, Eq, Debug)]
-struct Cursor { index: usize, }
+struct Cursor {
+    index: usize,
+}
 
 #[derive(Clone)]
-struct Maze { nodes: Vec<Node> }
+struct Maze {
+    nodes: Vec<Node>,
+}
 
 impl Maze {
     fn get(&self, cursor: Cursor) -> &Node {
@@ -70,7 +78,7 @@ impl Maze {
     fn add_dir(&mut self, cursor: Cursor, dir: Direction, node_type: NodeType) -> Cursor {
         let node = *self.get(cursor);
         let new_node = new_node(node_type);
-        let ret_cursor = Cursor {index: self.nodes.len()};
+        let ret_cursor = Cursor { index: self.nodes.len() };
         self.nodes.push(update_node_in_dir(new_node, reverse_dir(dir), &cursor));
         self.nodes[cursor.index] = update_node_in_dir(node, dir, &ret_cursor);
         ret_cursor
@@ -86,66 +94,92 @@ fn reverse_dir(dir: Direction) -> Direction {
     }
 }
 
-fn update_node_in_dir(node: Node, dir: Direction, cursor: & Cursor) -> Node {
+fn update_node_in_dir(node: Node, dir: Direction, cursor: &Cursor) -> Node {
     let new_cursor = Cursor { index: cursor.index };
     match dir {
-        Direction::Up => Node {up: Some(new_cursor), .. node},
-        Direction::Down => Node {down: Some(new_cursor), .. node},
-        Direction::Left => Node {left: Some(new_cursor), .. node},
-        Direction::Right => Node {right: Some(new_cursor), .. node},
+        Direction::Up => Node { up: Some(new_cursor), ..node },
+        Direction::Down => Node { down: Some(new_cursor), ..node },
+        Direction::Left => Node { left: Some(new_cursor), ..node },
+        Direction::Right => Node { right: Some(new_cursor), ..node },
     }
 }
 
 fn new_node(n_type: NodeType) -> Node {
     match n_type {
-        NodeType::Regular => Node {up: None, down: None, left: None, right: None, n_type: NodeType::Regular},
-        NodeType::End => Node {up: None, down: None, left: None, right: None, n_type: NodeType::End},
+        NodeType::Regular => {
+            Node {
+                up: None,
+                down: None,
+                left: None,
+                right: None,
+                n_type: NodeType::Regular,
+            }
+        }
+        NodeType::End => {
+            Node {
+                up: None,
+                down: None,
+                left: None,
+                right: None,
+                n_type: NodeType::End,
+            }
+        }
         NodeType::Start => panic!("This is impossible"),
     }
 }
 
 fn new() -> Maze {
-    Maze { nodes: vec![Node { up: None, down: None, left: None, right: None, n_type: NodeType::Start }] }
+    Maze {
+        nodes: vec![Node {
+                        up: None,
+                        down: None,
+                        left: None,
+                        right: None,
+                        n_type: NodeType::Start,
+                    }],
+    }
 }
 
 fn get_root() -> Cursor {
-    Cursor {index: 0}
+    Cursor { index: 0 }
 }
 
 fn make_plane(side_length: usize) -> Maze {
     let mut maze = new();
     let mut cursor;
     let y = 0;
-    for x in 0 .. side_length {
+    for x in 0..side_length {
         cursor = maze_coords(&maze, x, y);
         maze.add_dir(cursor, Direction::Right, NodeType::Regular);
-        for y in 0 .. side_length - 1 {
+        for y in 0..side_length - 1 {
             cursor = maze_coords(&maze, x, y);
             maze.add_dir(cursor, Direction::Down, NodeType::Regular);
         }
     }
     let mut cursor2;
-    for x in 0 .. (side_length - 1) {
-        for y in 1 .. side_length {
+    for x in 0..(side_length - 1) {
+        for y in 1..side_length {
             cursor = maze_coords(&maze, x, y);
-            cursor2 = maze_coords(&maze, x+1, y);
+            cursor2 = maze_coords(&maze, x + 1, y);
             maze = update_two_nodes(maze, cursor, cursor2, Direction::Right);
         }
     }
     cursor = maze_coords(&maze, side_length - 1, side_length - 1);
     let node = *maze.get(cursor);
-    maze.nodes[cursor.index] = Node {n_type: NodeType::End, .. node};
+    maze.nodes[cursor.index] = Node { n_type: NodeType::End, ..node };
     maze
 }
 
-fn update_two_nodes(maze: Maze, cursor_one: Cursor, cursor_two: Cursor, dir: Direction) -> Maze{
+fn update_two_nodes(maze: Maze, cursor_one: Cursor, cursor_two: Cursor, dir: Direction) -> Maze {
     let mut ret_maze = maze;
-    ret_maze.nodes[cursor_one.index] = update_node_in_dir(*ret_maze.get(cursor_one), dir, &cursor_two);
-    ret_maze.nodes[cursor_two.index] = update_node_in_dir(*ret_maze.get(cursor_two), reverse_dir(dir), &cursor_one);
+    ret_maze.nodes[cursor_one.index] =
+        update_node_in_dir(*ret_maze.get(cursor_one), dir, &cursor_two);
+    ret_maze.nodes[cursor_two.index] =
+        update_node_in_dir(*ret_maze.get(cursor_two), reverse_dir(dir), &cursor_one);
     ret_maze
 }
 
-fn maze_next(maze: & Maze, cursor: Cursor, dir: Direction) -> Cursor {
+fn maze_next(maze: &Maze, cursor: Cursor, dir: Direction) -> Cursor {
     let node = maze.get(cursor);
     match dir {
         Direction::Up => node.up.expect("Invalid up"),
@@ -155,16 +189,19 @@ fn maze_next(maze: & Maze, cursor: Cursor, dir: Direction) -> Cursor {
     }
 }
 
-fn maze_coords(maze: & Maze, x: usize, y: usize) -> Cursor {
+fn maze_coords(maze: &Maze, x: usize, y: usize) -> Cursor {
     let mut cursor = get_root();
-    for _ in 0 .. x {
+    for _ in 0..x {
         cursor = maze_next(maze, cursor, Direction::Right);
     }
-    for _ in 0 .. y {
+    for _ in 0..y {
         cursor = maze_next(maze, cursor, Direction::Down);
     }
     cursor
 }
+
+// TODO add wall-related utility functions
+// TODO add actual maze generation function
 
 fn main() {
     let maze = make_plane(4);
